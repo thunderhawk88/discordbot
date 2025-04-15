815bf5156245047ff4f7134eff77e991eee12aa3
thunderhawk88 <defcondude@gmail.com>
2025-04-15 17:15:55 -0600

    added month check for test holidqy

--- craxbot1/craxbot1.py
+++ craxbot1/craxbot1.py
@@ -989,7 +989,7 @@ async def called_every_min():
         embed.set_image(url=CraxData['fathersday']['Image'])
         await message_channel.send(CraxData['fathersday']['Message'], embed=embed)
 
-    if CurrentTime.day == CraxData['fathersday']['Day'] and CurrentTime.hour == CraxData['test']['Hour'] and CurrentTime.minute == 0 and CraxData['test']['Enable']: # Test
+    if CraxData['test']['Enable'] and CurrentTime.day == CraxData['test']['Day'] and CurrentTime.month == CraxData['test']['Month'] and CurrentTime.hour == CraxData['test']['Hour'] and CurrentTime.minute == 0: # Test
         message_channel = bot.get_channel(chan_tests)
         embed = discord.Embed(title='', description='')
         embed.set_image(url=CraxData['test']['Image'])
